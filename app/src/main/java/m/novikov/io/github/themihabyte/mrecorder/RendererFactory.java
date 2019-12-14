550da1fe8710198dd4d935de92111331a7437d4c
themihabyte <mihabayt@gmail.com>
2019-12-14 23:39:21 +0200

    SC

--- app/src/main/java/m/novikov/io/github/themihabyte/mrecorder/RendererFactory.java
+++ app/src/main/java/m/novikov/io/github/themihabyte/mrecorder/RendererFactory.java
@@ -1,7 +1,7 @@
 package m.novikov.io.github.themihabyte.mrecorder;
 
 public class RendererFactory {
-    public WaveFormRenderer createCurveWaveFormRenderer(int foreground, int background){
+    public WaveFormRenderer createCurveWaveFormRenderer(int foreground, int background) {
         return CurveWaveFormRenderer.newInstance(background, foreground);
     }
 }
